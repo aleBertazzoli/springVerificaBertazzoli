9dd00a332d3131b10607db40c4593b666ef8ed65
aleBertazzoli <alessandro.bertazzoli@marconirovereto.it>
2024-05-06 08:12:27 +0200

    inizio verifica...

---
+++ src/main/java/it/marconi/springbootverificabertazzoli/SpringbootVerificaBertazzoliApplication.java
@@ -0,0 +1,13 @@
+package it.marconi.springbootverificabertazzoli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootVerificaBertazzoliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootVerificaBertazzoliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/marconi/springbootverificabertazzoli/SpringbootVerificaBertazzoliApplicationTests.java
@@ -0,0 +1,13 @@
+package it.marconi.springbootverificabertazzoli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootVerificaBertazzoliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
